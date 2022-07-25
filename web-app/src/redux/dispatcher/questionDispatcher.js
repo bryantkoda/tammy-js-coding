2f550df3cd0c628babba2e01c5c15377352fcc5b
Bryant <bryant@kodait.com>
2022-07-26 09:44:50 +1200

    change to use online

--- web-app/src/redux/dispatcher/questionDispatcher.js
+++ web-app/src/redux/dispatcher/questionDispatcher.js
@@ -2,9 +2,10 @@ import questionApiService from "../../service/api/questionApiService";
 import { QuestionAction } from "../action/questionAction";
 
 class QuestionDispatcher {
-  
+
   loadQuestions(dispatch) {
     questionApiService.loadQuestions((res) => {
+      console.log(res)
       dispatch({
         type: QuestionAction.LOAD_QUESTIONS_SUCESSFUL,
         data: res
--- web-app/src/service/api/questionApiService.js
+++ web-app/src/service/api/questionApiService.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-const SERVER_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
+// const SERVER_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
+const SERVER_URL = "https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean";
 class QuestionApiService {
   async loadQuestions(onSuccess, onFailed) {
-    await axios.get(`${SERVER_URL}/api/questions`)
+    await axios.get(SERVER_URL)
+      .then(({ data }) => data)
       .then(res => {
-        onSuccess(res.data)
+        onSuccess(res.results)
       })
   }
 }
